b9247a8694ed76dba001b9f717c457feb2fae1f4
Alejandro Alcalde <algui91@gmail.com>
2018-11-05 17:14:52 +0100

    Fixin log test dependency

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   // Libraries
   lazy val scalaTest  = "org.scalatest" %% "scalatest" % "3.0.4" % "test"
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
-  lazy val loggers    = "ch.qos.logback" % "logback-classic" % "1.2.3" % "test"
+  lazy val loggers    = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
   // Test
   val testDeps =
