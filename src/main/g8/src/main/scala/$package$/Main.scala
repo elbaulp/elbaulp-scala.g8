79efca18cb4c6e0c48073971c47b49646113c33b
Alejandro Alcalde <algui91@gmail.com>
2018-11-05 17:10:29 +0100

    Added log4j

--- src/main/g8/src/main/scala/$package$/Main.scala
+++ src/main/g8/src/main/scala/$package$/Main.scala
@@ -1,6 +1,12 @@
 package $package$
 
+import org.slf4j.LoggerFactory
+
 object Hello extends Greeting with App {
+
+  private[this] val log = LoggerFactory.getLogger(Hello.getClass.getSimpleName)
+
+  log.debug(greeting)
   println(greeting)
 }
 
