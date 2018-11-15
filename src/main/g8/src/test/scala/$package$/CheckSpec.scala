8dc4bd20e8c8ee3bfd0e4cc5709c9b3512f2724c
Alejandro Alcalde <algui91@gmail.com>
2018-11-15 19:36:06 +0100

    Fully supporting scalatest syntax for scalaCheck

--- src/main/g8/src/test/scala/$package$/CheckSpec.scala
+++ src/main/g8/src/test/scala/$package$/CheckSpec.scala
@@ -1,6 +1,6 @@
 package $package$
 
-import org.scalatest.PropSpec
-import org.scalatest.prop.Checkers
+import org.scalatest.{ Matchers, PropSpec }
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
-abstract class CheckSpec extends PropSpec with Checkers
+abstract class CheckSpec extends PropSpec with GeneratorDrivenPropertyChecks with Matchers
