3629df8f9f832a0e2db6d34ac1c87de0ff6f59ca
Alejandro Alcalde <algui91@gmail.com>
2019-10-05 18:12:02 +0200

    Updated test dependencies
    Signed-off-by: Alejandro Alcalde <algui91@gmail.com>

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 object Dependencies {
 
   // Libraries
-  lazy val scalaTest  = "org.scalatest" %% "scalatest" % "3.0.4" % "test"
-  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
+  lazy val scalaTest  = "org.scalatest" %% "scalatest" % "3.2.0-M1" % "test"
+  lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.2" % "test"
   lazy val loggers    = "ch.qos.logback" % "logback-classic" % "1.2.3"
 
   // Test
